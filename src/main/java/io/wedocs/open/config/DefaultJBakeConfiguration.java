e0b023592e9541d61f9ce6f2aef3202f71dfb80f
wangko27 <845885222@qq.com>
2019-11-01 11:24:10 +0800

    不要的配置文件

--- src/main/java/io/wedocs/open/config/DefaultJBakeConfiguration.java
+++ src/main/java/io/wedocs/open/config/DefaultJBakeConfiguration.java
@@ -17,10 +17,7 @@ import org.springframework.context.annotation.Configuration;
 public class DefaultJBakeConfiguration {
 
     private String SOURCE_FOLDER_KEY = "sourceFolder";
-    private String DESTINATION_FOLDER_KEY = "destinationFolder";
-    private String ASSET_FOLDER_KEY = "assetFolder";
     private String TEMPLATE_FOLDER_KEY = "templateFolder";
-    private String CONTENT_FOLDER_KEY = "contentFolder";
     private Pattern TEMPLATE_DOC_PATTERN = Pattern.compile("(?:template\\.)([a-zA-Z0-9-_]+)(?:\\.file)");
     private String DOCTYPE_FILE_POSTFIX = ".file";
     private String DOCTYPE_EXTENSION_POSTFIX = ".extension";
@@ -82,11 +79,6 @@ public class DefaultJBakeConfiguration {
         return new File(new File(System.getProperty("user.dir")), destinationFolderName);
     }
 
-    public static final String DRAFT_SUFFIX = "draft.suffix";
-    public static final String FEED_FILE = "feed.file";
-    public static final String HEADER_SEPARATOR = "header.separator";
-    public static final String INDEX_FILE = "index.file";
-
     @Setter
     @Getter
     @Value("${markdown.extensions}")
@@ -99,18 +91,10 @@ public class DefaultJBakeConfiguration {
 
     public static final String PAGINATE_INDEX = "index.paginate";
     public static final String POSTS_PER_PAGE = "index.posts_per_page";
-    public static final String RENDER_ARCHIVE = "render.archive";
-    public static final String RENDER_FEED = "render.feed";
     public static final String RENDER_INDEX = "render.index";
-    public static final String RENDER_SITEMAP = "render.sitemap";
-    public static final String RENDER_TAGS = "render.tags";
-    public static final String RENDER_TAGS_INDEX = "render.tagsindex";
-    public static final String RENDER_ENCODING = "render.encoding";
+
     public static final String SERVER_PORT = "server.port";
     public static final String SITE_HOST = "site.host";
-    public static final String SITEMAP_FILE = "sitemap.file";
-    public static final String TAG_SANITIZE = "tag.sanitize";
-    public static final String TAG_PATH = "tag.path";
     public static final String TEMPLATE_FOLDER = "template.folder";
     public static final String TEMPLATE_ENCODING = "template.encoding";
     public static final String THYMELEAF_LOCALE = "thymeleaf.locale";
