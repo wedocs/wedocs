66f6e304a531e5136a1a61a337a94e32cdce8b56
wangko27 <845885222@qq.com>
2019-11-04 16:57:38 +0800

    summary解析策略

---
+++ src/main/java/io/wedocs/open/parser/SummaryParser.java
@@ -0,0 +1,10 @@
+package io.wedocs.open.parser;
+
+/**
+ * summary解析器
+ * <p>经过研究gitbook源码产生的新灵感，由于解析summary的格式非常复发,所以采用先转换给markdown在转成java对象</p>
+ * Created by wangkun23 on 2019/11/4.
+ */
+public class SummaryParser {
+
+}
