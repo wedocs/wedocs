1ded3453ee5bea6430338928d9eb1edb09a5192a
wangko27 <845885222@qq.com>
2019-11-04 17:36:27 +0800

    md

--- docs/Index.md
+++ docs/Index.md
@@ -1,1 +1,5 @@
-## index
+* [前言](README.md)
+
+* [第1章 介绍](chapter/1 Introduction.md)
+    * [1.1 LoRaWAN Classes](chapter/1 Introduction.md#1.1)
+    * [1.2 文档约定](chapter/1 Introduction.md#1.2)
--- src/test/java/io/wedocs/open/AppTest.java
+++ src/test/java/io/wedocs/open/AppTest.java
@@ -1,6 +1,7 @@
 package io.wedocs.open;
 
 import com.vladsch.flexmark.Extension;
+import com.vladsch.flexmark.ext.autolink.AutolinkExtension;
 import com.vladsch.flexmark.ext.tables.TablesExtension;
 import com.vladsch.flexmark.html.HtmlRenderer;
 import com.vladsch.flexmark.parser.Parser;
@@ -9,6 +10,7 @@ import com.vladsch.flexmark.profiles.pegdown.Extensions;
 import com.vladsch.flexmark.profiles.pegdown.PegdownOptionsAdapter;
 import com.vladsch.flexmark.util.ast.Node;
 import com.vladsch.flexmark.util.options.DataHolder;
+import com.vladsch.flexmark.util.options.MutableDataHolder;
 import com.vladsch.flexmark.util.options.MutableDataSet;
 import org.apache.commons.io.FileUtils;
 import org.junit.Test;
@@ -22,6 +24,7 @@ import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Map;
 
 /**
@@ -47,7 +50,12 @@ public class AppTest {
         //options.setFrom(ParserEmulationProfile.MARKDOWN);
         //options.set(Parser.EXTENSIONS, Arrays.asList(new Extension[] { TablesExtension.create()}));
 
-        DataHolder options = PegdownOptionsAdapter.flexmarkOptions(Extensions.ALL);
+        MutableDataHolder options = new MutableDataSet();
+        options.setFrom(ParserEmulationProfile.MARKDOWN);
+        // enable table parse!
+        options.set(Parser.EXTENSIONS, Arrays.asList(TablesExtension.create()));
+
+        // DataHolder options = PegdownOptionsAdapter.flexmarkOptions(Extensions.ANCHORLINKS);
 
 
         Parser parser = Parser.builder(options).build();
@@ -59,4 +67,18 @@ public class AppTest {
         String html = renderer.render(document);  // "<p>This is <em>Sparta</em></p>\n"
         System.out.println(html);
     }
+
+
+    @Test
+    public void builder() {
+        MutableDataSet options = new MutableDataSet();
+
+        Parser parser = Parser.builder(options).build();
+        HtmlRenderer renderer = HtmlRenderer.builder(options).build();
+
+        Node document = parser.parse("This is [link](https://github.com/freewind-demos/java-flexmark-java-demo/blob/master/src/main/java/demo/Hello.java)");
+        String html = renderer.render(document);
+        System.out.println(html);
+    }
+
 }
